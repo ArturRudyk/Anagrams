9b5519d079fd137f2d479b918c2c93bb7f734291
ArturRudyk <artur.rudyk@mail.ru>
2017-01-22 17:56:57 +0200

    Anagrams

--- Anagrams.java
+++ Anagrams.java
@@ -1,33 +1,24 @@
 package standart;
 
 public class Anagrams {
-     public static void main (String args[]){
-    	 
-    	 
-    	 String s= "Stroka slow razdelennaya probelami";
-    	 StringBuffer sb = new StringBuffer();
-    	 String fin= new String();
-    	 int a=0;
-    	   	 
-    	 for (int i=0; i<s.length();i++){
-    		 
-    		if(  s.charAt(i) == ' '){    			
-    		    sb=new StringBuffer(s.substring(a, i));
-    		    sb.reverse();
-    		    fin +=sb+" ";
-    		    a=i+1;   		 
+     public static void main (String args[]) {
+    	 String string = "Stroka slow razdelennaya probelami";
+    	 StringBuffer stringBuffer = new StringBuffer();
+    	 String resultString = "";
+    	 int beginOfNewWord = 0;
+    	 for (int i = 0; i < string.length(); i++) {
+    		if (string.charAt(i) == ' ') {    			
+    		    stringBuffer = new StringBuffer(string.substring(beginOfNewWord, i));
+    		    stringBuffer.reverse();
+    		    resultString += stringBuffer + " ";
+    		    beginOfNewWord = i + 1;   		 
     		}
-    		
-    		if (i==s.length()-1){
-    			sb=new StringBuffer(s.substring(a, s.length()));
-    		    sb.reverse();
-    		    fin +=sb+" ";
-    		      
+    		if (i == string.length() - 1) {
+    			stringBuffer = new StringBuffer(string.substring(beginOfNewWord, string.length()));
+    		    stringBuffer.reverse();
+    		    resultString += stringBuffer + " ";
     		}
     	 }
-    	 
-    	 
-      	 System.out.println(fin);
-
+      	 System.out.println(resultString);
      }
 }
